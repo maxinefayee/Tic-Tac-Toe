ab0ef7d510be7d24a08ec2b2018dfea055164866
Maxine Faye S. Bella <maxinebella02@gmail.com:)>
2021-05-27 20:29:56 +0800

    third commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn1TTT = new System.Windows.Forms.Button();
+            this.Btn3TTT = new System.Windows.Forms.Button();
+            this.Btn2TTT = new System.Windows.Forms.Button();
+            this.Btn4TTT = new System.Windows.Forms.Button();
+            this.Btn5TTT = new System.Windows.Forms.Button();
+            this.Btn6TTT = new System.Windows.Forms.Button();
+            this.Btn7TTT = new System.Windows.Forms.Button();
+            this.Btn8TTT = new System.Windows.Forms.Button();
+            this.Btn9TTT = new System.Windows.Forms.Button();
+            this.TTText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn1TTT
+            // 
+            this.Btn1TTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn1TTT.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Btn1TTT.Location = new System.Drawing.Point(9, 51);
+            this.Btn1TTT.Name = "Btn1TTT";
+            this.Btn1TTT.Size = new System.Drawing.Size(113, 109);
+            this.Btn1TTT.TabIndex = 0;
+            this.Btn1TTT.UseVisualStyleBackColor = true;
+            this.Btn1TTT.Click += new System.EventHandler(this.Button_click);
+            // 
+            // Btn3TTT
+            // 
+            this.Btn3TTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn3TTT.Location = new System.Drawing.Point(245, 51);
+            this.Btn3TTT.Name = "Btn3TTT";
+            this.Btn3TTT.Size = new System.Drawing.Size(113, 109);
+            this.Btn3TTT.TabIndex = 1;
+            this.Btn3TTT.UseVisualStyleBackColor = true;
+            this.Btn3TTT.Click += new System.EventHandler(this.Button_click);
+            // 
+            // Btn2TTT
+            // 
+            this.Btn2TTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn2TTT.Location = new System.Drawing.Point(128, 51);
+            this.Btn2TTT.Name = "Btn2TTT";
+            this.Btn2TTT.Size = new System.Drawing.Size(113, 109);
+            this.Btn2TTT.TabIndex = 2;
+            this.Btn2TTT.UseVisualStyleBackColor = true;
+            this.Btn2TTT.Click += new System.EventHandler(this.Button_click);
+            // 
+            // Btn4TTT
+            // 
+            this.Btn4TTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn4TTT.Location = new System.Drawing.Point(9, 166);
+            this.Btn4TTT.Name = "Btn4TTT";
+            this.Btn4TTT.Size = new System.Drawing.Size(113, 109);
+            this.Btn4TTT.TabIndex = 3;
+            this.Btn4TTT.UseVisualStyleBackColor = true;
+            this.Btn4TTT.Click += new System.EventHandler(this.Button_click);
+            // 
+            // Btn5TTT
+            // 
+            this.Btn5TTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn5TTT.Location = new System.Drawing.Point(128, 166);
+            this.Btn5TTT.Name = "Btn5TTT";
+            this.Btn5TTT.Size = new System.Drawing.Size(113, 109);
+            this.Btn5TTT.TabIndex = 4;
+            this.Btn5TTT.UseVisualStyleBackColor = true;
+            this.Btn5TTT.Click += new System.EventHandler(this.Button_click);
+            // 
+            // Btn6TTT
+            // 
+            this.Btn6TTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn6TTT.Location = new System.Drawing.Point(245, 166);
+            this.Btn6TTT.Name = "Btn6TTT";
+            this.Btn6TTT.Size = new System.Drawing.Size(113, 109);
+            this.Btn6TTT.TabIndex = 5;
+            this.Btn6TTT.UseVisualStyleBackColor = true;
+            this.Btn6TTT.Click += new System.EventHandler(this.Button_click);
+            // 
+            // Btn7TTT
+            // 
+            this.Btn7TTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn7TTT.Location = new System.Drawing.Point(9, 281);
+            this.Btn7TTT.Name = "Btn7TTT";
+            this.Btn7TTT.Size = new System.Drawing.Size(113, 109);
+            this.Btn7TTT.TabIndex = 6;
+            this.Btn7TTT.UseVisualStyleBackColor = true;
+            this.Btn7TTT.Click += new System.EventHandler(this.Button_click);
+            // 
+            // Btn8TTT
+            // 
+            this.Btn8TTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn8TTT.Location = new System.Drawing.Point(128, 281);
+            this.Btn8TTT.Name = "Btn8TTT";
+            this.Btn8TTT.Size = new System.Drawing.Size(113, 109);
+            this.Btn8TTT.TabIndex = 7;
+            this.Btn8TTT.UseVisualStyleBackColor = true;
+            this.Btn8TTT.Click += new System.EventHandler(this.Button_click);
+            // 
+            // Btn9TTT
+            // 
+            this.Btn9TTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn9TTT.Location = new System.Drawing.Point(247, 281);
+            this.Btn9TTT.Name = "Btn9TTT";
+            this.Btn9TTT.Size = new System.Drawing.Size(113, 109);
+            this.Btn9TTT.TabIndex = 8;
+            this.Btn9TTT.UseVisualStyleBackColor = true;
+            this.Btn9TTT.Click += new System.EventHandler(this.Button_click);
+            // 
+            // TTText
+            // 
+            this.TTText.AutoSize = true;
+            this.TTText.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TTText.ForeColor = System.Drawing.Color.White;
+            this.TTText.Location = new System.Drawing.Point(95, 9);
+            this.TTText.Name = "TTText";
+            this.TTText.Size = new System.Drawing.Size(181, 39);
+            this.TTText.TabIndex = 9;
+            this.TTText.Text = "Tic Tac Toe";
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(368, 402);
+            this.Controls.Add(this.TTText);
+            this.Controls.Add(this.Btn9TTT);
+            this.Controls.Add(this.Btn8TTT);
+            this.Controls.Add(this.Btn7TTT);
+            this.Controls.Add(this.Btn6TTT);
+            this.Controls.Add(this.Btn5TTT);
+            this.Controls.Add(this.Btn4TTT);
+            this.Controls.Add(this.Btn2TTT);
+            this.Controls.Add(this.Btn3TTT);
+            this.Controls.Add(this.Btn1TTT);
+            this.Name = "TicTacToe";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn1TTT;
+        private System.Windows.Forms.Button Btn3TTT;
+        private System.Windows.Forms.Button Btn2TTT;
+        private System.Windows.Forms.Button Btn4TTT;
+        private System.Windows.Forms.Button Btn5TTT;
+        private System.Windows.Forms.Button Btn6TTT;
+        private System.Windows.Forms.Button Btn7TTT;
+        private System.Windows.Forms.Button Btn8TTT;
+        private System.Windows.Forms.Button Btn9TTT;
+        private System.Windows.Forms.Label TTText;
+    }
+}
+
